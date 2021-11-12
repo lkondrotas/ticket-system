581f90a7c0c3007f8cf5819861d174835b8733c8
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-11-12 05:41:12 +0200

    Created and Styled NAV

--- ticket-system/src/index.js
+++ ticket-system/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import Nav from "./containers/nav";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavTop from "./containers/nav";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nav />
+    <NavTop />
   </React.StrictMode>,
   document.getElementById("nav")
 );
