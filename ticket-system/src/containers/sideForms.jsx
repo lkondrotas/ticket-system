18eeefcb8210829163b509ad5f0259ed26524d11
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-11-20 03:32:43 +0200

    Installed Bootstrap Typeahead

--- ticket-system/src/containers/sideForms.jsx
+++ ticket-system/src/containers/sideForms.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { Form, FloatingLabel } from "react-bootstrap";
+import { Typeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
 
 class SideForms extends React.Component {
-  state = {};
+  state = {
+    selectedValues: [],
+  };
   render() {
     const { label, options, placeHolder } = this.props;
     return (
       <React.Fragment>
-        <FloatingLabel className="text-secondary  pb-1" label={label}>
+        {/* <FloatingLabel className="text-white pb-1" label={label}>
           <Form.Control
             as="select"
             data-live-search
-            className="border-0 rounded-0 bg-dark text-light"
+            multiple
+            value={this.state.selectedValues}
+            className="border-0 fields rounded-0 bg-dark text-light"
             placeholder={placeHolder}
           >
             {options.map((option) => (
               <option>{option}</option>
             ))}
           </Form.Control>
-        </FloatingLabel>
+        </FloatingLabel> */}
+        <Typeahead
+          labelKey="name"
+          multiple
+          options={options}
+          placeholder={placeHolder}
+        />
       </React.Fragment>
     );
   }
