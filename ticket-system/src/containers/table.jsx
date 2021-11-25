b81e0814595f1681bbdf7eb50f2aadd00f5b3200
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-11-25 02:55:44 +0200

    Fixed positions or search/header and bottom of table

--- ticket-system/src/containers/table.jsx
+++ ticket-system/src/containers/table.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Table, Button, InputGroup, FormControl } from "react-bootstrap";
+import {
+  Table,
+  Button,
+  InputGroup,
+  FormControl,
+  Container,
+} from "react-bootstrap";
 import {
   useTable,
   usePagination,
@@ -16,7 +22,7 @@ export default function CustomTable({ columns, data }) {
     {
       columns,
       data,
-      initialState: { pageIndex: 0, pageSize: 15 },
+      initialState: { pageIndex: 0, pageSize: 20 },
     },
     useFilters,
     useGlobalFilter,
@@ -45,58 +51,72 @@ export default function CustomTable({ columns, data }) {
 
   return (
     <React.Fragment>
-      <Table striped hover {...getTableProps()}>
-        <thead>
-          <tr>
-            <GlobalFilter
-              preGlobalFilteredRows={preGlobalFilteredRows}
-              globalFilter={state.globalFilter}
-              setGlobalFilter={setGlobalFilter}
-            />
-          </tr>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+      <Container fluid className="overflow-auto" style={{ maxHeight: "80vh" }}>
+        <Table striped hover {...getTableProps()}>
+          <thead className="sticky-top bg-white shadow">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+            ))}
+          </thead>
+
+          <tbody {...getTableBodyProps()}>
+            {page.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
       {/* Buttons to go through pages */}
-      <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
-        {"<"}
-      </Button>{" "}
-      <Button onClick={() => nextPage()} disabled={!canNextPage}>
-        {">"}
-      </Button>{" "}
-      {/* Selection to how many per page to display */}
-      <select
-        value={pageSize}
-        onChange={(e) => {
-          setPageSize(Number(e.target.value));
-        }}
-      >
-        {[20, 50, 100].map((pageSize) => (
-          <option key={pageSize} value={pageSize}>
-            Show {pageSize}
-          </option>
-        ))}
-      </select>
+      <Container className="pt-2 justify-content-end d-flex">
+        <Button
+          className="m-1"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          {"<"}
+        </Button>{" "}
+        <Button
+          className="m-1"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          {">"}
+        </Button>{" "}
+        {/* Selection to how many per page to display */}
+        <select
+          className="m-1"
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[20, 50, 100].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </Container>
     </React.Fragment>
   );
 }
@@ -112,7 +132,7 @@ function GlobalFilter({
   }, 0);
 
   return (
-    <InputGroup variant="primary">
+    <InputGroup className="pb-2" variant="primary">
       <InputGroup.Text>Search</InputGroup.Text>
       <FormControl
         value={value || ""}
