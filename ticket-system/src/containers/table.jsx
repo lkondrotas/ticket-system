9dad6ba33d91c51d4b0a6fd35d020a34939b7651
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-11-28 20:18:18 +0200

    adjusted pagenation

--- ticket-system/src/containers/table.jsx
+++ ticket-system/src/containers/table.jsx
@@ -38,8 +38,8 @@ export default function CustomTable({ columns, data }) {
     canPreviousPage,
     canNextPage,
     // pageOptions,
-    // pageCount,
-    // gotoPage,
+    pageCount,
+    gotoPage,
     nextPage,
     previousPage,
     setPageSize,
@@ -88,13 +88,27 @@ export default function CustomTable({ columns, data }) {
       </Container>
       {/* Buttons to go through pages */}
       <Container className="pt-2 justify-content-end d-flex">
+        {/* Goto First page */}
+        <Button
+          className="m-1"
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          {"First"}
+        </Button>
+        {/* Goto previous page */}
         <Button
           className="m-1"
           onClick={() => previousPage()}
           disabled={!canPreviousPage}
         >
           {"<"}
+        </Button>
+        {/* Current page */}
+        <Button className="m-1 btn-dark" disabled={true}>
+          {`${pageIndex + 1} of ${pageCount}`}
         </Button>{" "}
+        {/* Goto next page */}
         <Button
           className="m-1"
           onClick={() => nextPage()}
@@ -102,6 +116,14 @@ export default function CustomTable({ columns, data }) {
         >
           {">"}
         </Button>{" "}
+        {/* Goto last page */}
+        <Button
+          className="m-1"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          {"Last"}
+        </Button>
         {/* Selection to how many per page to display */}
         <select
           className="m-1"
