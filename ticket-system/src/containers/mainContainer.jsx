c445299474067eb7643eea2aa8189aa81edc5df3
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-12-02 03:34:15 +0200

    Added routes and cleaned up project

--- ticket-system/src/containers/mainContainer.jsx
+++ ticket-system/src/containers/mainContainer.jsx
@@ -1,17 +1,61 @@
-import { Container, Row, Col } from "react-bootstrap";
-import SideMenu from "./sideMenu";
-import Main from "./Main";
+import { Container, Row, Col, Collapse } from "react-bootstrap";
+import "../css/nav.css";
 import React from "react";
+import CustomTable from "../components/table";
+import data from "../data/data.json";
+import headers from "../data/headers.json";
+import arrow from "../images/arrow.svg";
+import arrow2 from "../images/arrow2.svg";
 
 class MainContainer extends React.Component {
-  state = {};
+  state = {
+    open: false,
+  };
 
   render() {
     return (
       <Container fluid>
         <Row>
-          <SideMenu />
-          <Main />
+          <React.Fragment>
+            <Collapse in={this.state.open} dimension="width">
+              <Col id="sideMenu" xs={2} className="vh-100 bg-secondary">
+                <div
+                  style={{ width: "calc((100vw / 12)*2)", height: "80px" }}
+                ></div>
+                {/* Place filers content here <Filers /> */}
+              </Col>
+            </Collapse>
+
+            <a
+              id="sideBar"
+              href="javascript:void(0)"
+              className="bg-secondary text-uppercase text-decoration-none text-nowrap"
+              onClick={() => {
+                this.state.open
+                  ? this.setState({ open: false })
+                  : this.setState({ open: true });
+              }}
+            >
+              <div>
+                <img
+                  src={this.state.open ? arrow : arrow2}
+                  alt="Arrow"
+                  className="closeImage"
+                />
+                <h2>filters | preferences</h2>
+              </div>
+            </a>
+          </React.Fragment>
+          <Col
+            style={{ maxHeight: "100vh", paddingTop: "75px" }}
+            className="overflow-auto"
+          >
+            <Container fluid>
+              <Row>
+                <CustomTable columns={headers} data={data} />
+              </Row>
+            </Container>
+          </Col>
         </Row>
       </Container>
     );
--- ticket-system/src/index.js
+++ ticket-system/src/index.js
@@ -2,18 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavTop from "./containers/nav";
-import MainContainer from "./containers/mainContainer";
+import AllTickets from "./containers/mainContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <NavTop />
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />}></Route> */}
+        <Route path="/" element={<AllTickets />}></Route>
+        {/* <Route path="mytickets" element={<MyTickets />}></Route>
+        <Route path="reports" element={<Reports />}></Route>
+        <Route path="exports" element={<Exports />}></Route>
+        <Route path="newticket" element={<NewTicket />}></Route>
+        <Route path="profile" element={<Profile />}></Route> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("nav")
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <MainContainer />
-  </React.StrictMode>,
-  document.getElementById("MainContainer")
+  document.getElementById("root")
 );
