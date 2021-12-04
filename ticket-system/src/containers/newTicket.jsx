6005c92a295740cb5932b7d29b5ea984e5e72951
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-12-04 04:42:29 +0200

    Did something, not sure what, but its almost working

--- ticket-system/src/containers/newTicket.jsx
+++ ticket-system/src/containers/newTicket.jsx
@@ -12,6 +12,7 @@ import "../css/nav.css";
 import React from "react";
 import arrow from "../images/arrow.svg";
 import arrow2 from "../images/arrow2.svg";
+import testMids from "../data/testMids.json";
 
 class NewTicket extends React.Component {
   state = {
@@ -21,11 +22,10 @@ class NewTicket extends React.Component {
   };
 
   value;
-  mi;
 
   render() {
     return (
-      <Container fluid as={Form}>
+      <Container fluid as={Form} method="_POST">
         <Row>
           <React.Fragment>
             <Collapse in={this.state.open} dimension="width">
@@ -42,9 +42,9 @@ class NewTicket extends React.Component {
                       onChange={(e) => (this.value = e.target.value)}
                     ></Form.Control>
                     <Button
-                      onClick={() =>
-                        this.setState({ mi: this.Merchantinfo(this.value) })
-                      }
+                    // onClick={() =>
+                    //   this.setState({ mi: this.Merchantinfo(this.value) })
+                    // }
                     >
                       Search
                     </Button>
@@ -77,7 +77,7 @@ class NewTicket extends React.Component {
           </React.Fragment>
           <Col style={{ paddingTop: "75px" }} className="overflow-auto">
             <Container fluid>
-              <Row></Row>
+              <Row>{this.MerchantSelection(2)}</Row>
             </Container>
           </Col>
         </Row>
@@ -85,48 +85,70 @@ class NewTicket extends React.Component {
     );
   }
 
-  Merchantinfo(value) {
-    if (value) {
-      return (
-        <React.Fragment>
-          <FloatingLabel label="DBA">
-            <Form.Control
-              className="rounded-0"
-              disabled
-              value="VILNIUS TEST ACCOUNT 4"
-            ></Form.Control>
-          </FloatingLabel>
-          <FloatingLabel label="MID/SN">
-            <Form.Control
-              className="rounded-0"
-              disabled
-              value="21224567"
-            ></Form.Control>
-          </FloatingLabel>
-          <FloatingLabel label="Email">
-            <Form.Control
-              className="rounded-0"
-              disabled
-              value="lkondrotas@shift4.com"
-            ></Form.Control>
-          </FloatingLabel>
-          <FloatingLabel label="Phone Number">
-            <Form.Control
-              className="rounded-0"
-              disabled
-              value="888-351-2808"
-            ></Form.Control>
-          </FloatingLabel>
-          <br />
-          <FloatingLabel label="Test">
-            <Form.Control
-              className="border-0 rounded-0"
-              disabled
-              value="Test Test"
-            ></Form.Control>
-          </FloatingLabel>
-        </React.Fragment>
-      );
+  MerchantSelection(value) {
+    let list = testMids.filter((test) => test.mid == value);
+    const merchants = list.map((listItem) => ({
+      dba: listItem.dba,
+      mid: listItem.mid,
+      email: listItem.email,
+      phone: listItem.phone,
+      tba: listItem.tba,
+    }));
+    return (
+      <Form.Control as="select" custom onChange={(e) => this.Merchantinfo(e)}>
+        <option value={null}>Test</option>
+        {merchants.map((merchant) => (
+          <option value={merchant}>{merchant.dba}</option>
+        ))}
+      </Form.Control>
+    );
+  }
+
+  Merchantinfo(merchant) {
+    console.log(merchant);
+    if (merchant) {
+      this.setState({
+        mi: (
+          <React.Fragment>
+            <FloatingLabel label="DBA">
+              <Form.Control
+                className="rounded-0"
+                disabled
+                value={merchant.dba}
+              ></Form.Control>
+            </FloatingLabel>
+            <FloatingLabel label="MID/SN">
+              <Form.Control
+                className="rounded-0"
+                disabled
+                value={merchant.mid}
+              ></Form.Control>
+            </FloatingLabel>
+            <FloatingLabel label="Email">
+              <Form.Control
+                className="rounded-0"
+                disabled
+                value={merchant.email}
+              ></Form.Control>
+            </FloatingLabel>
+            <FloatingLabel label="Phone Number">
+              <Form.Control
+                className="rounded-0"
+                disabled
+                value={merchant.phone}
+              ></Form.Control>
+            </FloatingLabel>
+            <br />
+            <FloatingLabel label="Test">
+              <Form.Control
+                className="border-0 rounded-0"
+                disabled
+                value={merchant.tba}
+              ></Form.Control>
+            </FloatingLabel>
+          </React.Fragment>
+        ),
+      });
     }
   }
 }
