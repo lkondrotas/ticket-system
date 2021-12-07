7f1bc963f19ac9b89998661119d65b78076b8ae0
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2021-12-07 04:33:24 +0200

    Created form

--- ticket-system/src/containers/newTicket.jsx
+++ ticket-system/src/containers/newTicket.jsx
@@ -7,6 +7,7 @@ import {
   FloatingLabel,
   InputGroup,
   Button,
+  Card,
 } from "react-bootstrap";
 import "../css/nav.css";
 import React from "react";
@@ -105,19 +106,193 @@ class NewTicket extends React.Component {
               </div>
             </a>
           </React.Fragment>
-          <Col style={{ paddingTop: "75px" }}>
+          <Col style={{ padding: "67px 0px 0px 0px" }}>
             <Container fluid>
-              <Row className="overflow-scroll" style={{ maxHeight: "90vh" }}>
-                <React.Fragment>
-                  <Form.Label>Test</Form.Label>
-                  <Form.Control as="textarea"></Form.Control>
-                </React.Fragment>
+              <Row className="overflow-auto pt-2" style={{ maxHeight: "92vh" }}>
+                <Form.Group className="pt-3 rounded mb-5">
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text className="ig-width">
+                      Subject
+                    </InputGroup.Text>
+                    <Form.Control placeholder="Subject"></Form.Control>
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text className="ig-width">
+                      Description
+                    </InputGroup.Text>
+                    <Form.Control
+                      as="textarea"
+                      placeholder="Write description of the issue here..."
+                      style={{ minHeight: "300px", maxHeight: "600px" }}
+                    ></Form.Control>
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text className="ig-width">
+                      Date and time
+                    </InputGroup.Text>
+                    <Form.Control placeholder=""></Form.Control>
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text className="ig-width">
+                      Error message
+                    </InputGroup.Text>
+                    <Form.Control placeholder="Error message"></Form.Control>
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text className="ig-width">
+                      Action plan
+                    </InputGroup.Text>
+                    <Form.Control
+                      as="textarea"
+                      placeholder="Action plan"
+                      style={{ minHeight: "70px", maxHeight: "200px" }}
+                    ></Form.Control>
+                  </InputGroup>
+                </Form.Group>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text className="ig-width">
+                    Solution
+                  </InputGroup.Text>
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Write solution how you fixed this issue..."
+                    style={{ minHeight: "100px", maxHeight: "200px" }}
+                  ></Form.Control>
+                </InputGroup>
+                <Form.Group>
+                  <Form.Text>Notes</Form.Text>
+                  <InputGroup>
+                    <Card>
+                      <Card.Header className="align-content-end">
+                        <span className="fw-bold">Lukas Kondrotas</span>{" "}
+                        <span className="fw-light">12/7/2021 3:13 PM</span>
+                      </Card.Header>
+                      <Card.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Doloremque quidem nulla autem necessitatibus
+                        tempore quos quas magni consequuntur veritatis
+                        accusantium laudantium ea blanditiis quasi dolorum, nisi
+                        quaerat fuga obcaecati aliquid. Lorem ipsum, dolor sit
+                        amet consectetur adipisicing elit. Voluptate dolorem
+                        maiores ipsa ex labore aperiam in odit iusto,
+                        laboriosam, molestias nesciunt sit et debitis unde est
+                        ea corporis fuga eligendi? Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Tempora, laboriosam at.
+                        Voluptatibus laborum velit sunt ea itaque, consectetur
+                        architecto aliquam asperiores excepturi, voluptas eos.
+                        Corrupti facere error voluptatum voluptates voluptate.
+                      </Card.Body>
+                    </Card>
+                  </InputGroup>
+                  <InputGroup>
+                    <Card>
+                      <Card.Header>
+                        <span className="fw-bold">Donatas Ladysas</span>{" "}
+                        <span className="fw-light">12/7/2021 4:55 PM</span>
+                      </Card.Header>
+                      <Card.Body>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Asperiores nobis incidunt dicta dolorum dolores error
+                        iure repudiandae numquam, nemo quae distinctio suscipit
+                        esse provident veniam debitis recusandae possimus odit
+                        nulla? Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Ipsam blanditiis non quia dolor
+                        perspiciatis illum alias numquam optio ducimus nisi
+                        ratione rerum mollitia, nihil ea officiis doloremque
+                        dolore! At, laboriosam? Lorem, ipsum dolor sit amet
+                        consectetur adipisicing elit. Autem sit, corporis
+                        officia, enim sunt perspiciatis iste ipsa laborum
+                        facilis sequi vitae voluptate molestias exercitationem
+                        ducimus veritatis quam maxime. Similique, repellat!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aperiam vel fugit excepturi, tempora soluta dolorem, hic
+                        mollitia repellat quae pariatur veniam provident est,
+                        aliquam nesciunt? Quasi asperiores ex suscipit iusto!
+                      </Card.Body>
+                    </Card>
+                  </InputGroup>
+                  <InputGroup>
+                    <Form.Control
+                      as="textarea"
+                      style={{ minHeight: "150px" }}
+                    ></Form.Control>
+                    <Button>Add Note</Button>
+                  </InputGroup>
+                </Form.Group>
               </Row>
             </Container>
           </Col>
           <Col xl={2} xs={3} className="vh-100 bg-secondary shadow-sm">
             <div style={{ height: "80px" }}></div>
-            <div>Test</div>
+            <Form.Group className="mb-2">
+              <Form.Text style={{ color: "#fff" }}>
+                Ticket Information
+              </Form.Text>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Created
+                </InputGroup.Text>
+                <Form.Control as="text">12/7/2021 3:01 PM</Form.Control>
+              </InputGroup>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Updated
+                </InputGroup.Text>
+                <Form.Control as="text">12/7/2021 4:55 PM</Form.Control>
+              </InputGroup>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Source
+                </InputGroup.Text>
+                <Form.Control as="select">
+                  <option>Phone</option>
+                  <option>Chat</option>
+                  <option>Email</option>
+                </Form.Control>
+              </InputGroup>
+            </Form.Group>
+            <Form.Group>
+              <Form.Text style={{ color: "#fff" }}>
+                Department and asignee
+              </Form.Text>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Created
+                </InputGroup.Text>
+                <Form.Control as="text">Lukas Kondrotas</Form.Control>
+              </InputGroup>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Dept
+                </InputGroup.Text>
+                <Form.Control as="select">
+                  <option>Advance Support</option>
+                  <option>Customer Service</option>
+                  <option>Technical Services</option>
+                </Form.Control>
+              </InputGroup>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Dept
+                </InputGroup.Text>
+                <Form.Control as="select">
+                  <option>AS POS/PMS</option>
+                  <option>AS Advanced</option>
+                  <option>AS Audit</option>
+                </Form.Control>
+              </InputGroup>
+              <InputGroup className="mb-1">
+                <InputGroup.Text className="ig-width-side">
+                  Asignee
+                </InputGroup.Text>
+                <Form.Control as="select">
+                  <option>Lukas Kondrotas</option>
+                  <option>Donatas Ladyas</option>
+                  <option>Vardenis Pavardenis</option>
+                </Form.Control>
+              </InputGroup>
+            </Form.Group>
+            <Button className="col-12 btn-lg">Create ticket</Button>
           </Col>
         </Row>
       </Container>
