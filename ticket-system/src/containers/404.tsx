8ee272469b759ada024c47c5514c1d8469791e33
lkondrotas <92922410+lkondrotas@users.noreply.github.com>
2022-04-28 03:33:13 +0300

    Fixes

---
+++ ticket-system/src/containers/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class notFound extends React.Component {
+  render() {
+    return <h1 style={{ paddingTop: "100px" }}>404 Page not found</h1>;
+  }
+}
+
+export default notFound;
--- ticket-system/src/index.tsx
+++ ticket-system/src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavTop from "./containers/nav";
@@ -10,8 +9,8 @@ import Home from "./containers/home";
 import Reports from "./containers/reports";
 import Exports from "./containers/exports";
 import Profile from "./containers/profile";
-import Signin from "./containers/signin";
 import Example from "./containers/ticketExample"
+import NotFound from "./containers/404"
 
 const auth: boolean = true;
 
@@ -20,13 +19,15 @@ ReactDOM.render(
     <NavTop></NavTop>
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="alltickets" element={<AllTickets />} />
+      <Route path="tickets" element={<AllTickets />} />
       <Route path="mytickets" element={<MyTickets />}></Route>
       <Route path="reports" element={<Reports />}></Route>
       <Route path="exports" element={<Exports />}></Route>
       <Route path="tickets/newticket" element={<NewTicket />} />
       <Route path="tickets/123456" element={<Example />} />
       <Route path="profile" element={<Profile />}></Route>
+      <Route path="tickets/*" element={<Exports />}></Route> {/* 404 page for tickets */}
+      <Route path="*" element={<NotFound />}></Route> {/* 404 page */}
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
